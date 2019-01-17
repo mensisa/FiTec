c1b6d46e32b5246e6070267df1972b9f275d8599
OHHYEONJEONG <guswjd3927@naver.com>
2019-01-18 08:24:02 +0900

    Remove comment

--- arduino/FitecSensor/FitecSensor.ino
+++ arduino/FitecSensor/FitecSensor.ino
@@ -1,31 +1,3 @@
-/*#include <SoftwareSerial.h>
-int times;
-int rxPin = 3;
-int txPin = 2;
-SoftwareSerial hc06(rxPin, txPin);
-
-String message; //string that stores the incoming message
-char ch;
-void setup()
-{
- Serial.begin(9600); //set baud rate
- hc06.begin(9600); //set baud rate
-}
-
-void loop() {
- // put your main code here, to run repeatedly:
- if(Serial.available())
- {
-   ch = Serial.read();
-   hc06.write(ch);  
-   Serial.write(ch);
- }
-if(hc06.available())
- {
-   Serial.write(hc06.read());  
- }
-}*/
-
 #define TxD 14;
 #define RxD 15;
 #define BTSerial Serial3;
