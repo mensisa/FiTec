a973ccc76703e7bac2341d465e5c87798a53b32d
OHHYEONJEONG <guswjd3927@naver.com>
2019-01-18 08:16:56 +0900

    Complete Bluetooth Connection

--- arduino/FitecSensor/FitecSensor.ino
+++ arduino/FitecSensor/FitecSensor.ino
@@ -1,22 +1,45 @@
-#include <SoftwareSerial.h>
- 
-int Tx=6; //transport
-int Rx=7; //receive
- 
-SoftwareSerial btSerial(Tx, Rx);
- 
-void setup() 
+/*#include <SoftwareSerial.h>
+int times;
+int rxPin = 3;
+int txPin = 2;
+SoftwareSerial hc06(rxPin, txPin);
+
+String message; //string that stores the incoming message
+char ch;
+void setup()
 {
-  Serial.begin(9600);
-  btSerial.begin(9600);
+ Serial.begin(9600); //set baud rate
+ hc06.begin(9600); //set baud rate
 }
- 
-void loop()
-{
-  if (btSerial.available()) {       
-    Serial.write(btSerial.read());
-  }
-  if (Serial.available()) {         
-    btSerial.write(Serial.read());
-  }
+
+void loop() {
+ // put your main code here, to run repeatedly:
+ if(Serial.available())
+ {
+   ch = Serial.read();
+   hc06.write(ch);  
+   Serial.write(ch);
+ }
+if(hc06.available())
+ {
+   Serial.write(hc06.read());  
+ }
+}*/
+
+#define TxD 14;
+#define RxD 15;
+#define BTSerial Serial3;
+
+void setup(){
+  Serial.begin(9600);
+  Serial3.begin(9600);
+}
+
+void loop(){
+  if(Serial3.available()){
+    Serial.write(Serial3.read());
+  }
+  if(Serial.available()){
+    Serial3.write(Serial.read());
+  }
 }
