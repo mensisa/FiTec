b666bd56c7c8c7ff0d2e7f0741f3e2fe64ec74ac
Jakeone Im <jakeoneim@Jakeoneui-MacBookPro.local>
2019-01-17 17:01:47 -0500

    get rid of comment

--- android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
+++ android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.jakeoneim.fitec;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -83,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-/*
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(resultCode == Activity.RESULT_OK){
@@ -96,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext() , "" , Toast.LENGTH_LONG).show();
             finish();
         }
-    }*/
+    }
 
     public void setupService(){
         bluetooth.setupService();
